e7509e91d2714869e5e197314088a45737e831df
Austin <sales@automachinespecialties.com>
2024-11-15 16:45:49 -0800

    feat(admin): add notes, reorganize order

--- api/admin.py
+++ api/admin.py
@@ -5,6 +5,6 @@ from .models        import TimeEntry
 
 @admin.register(TimeEntry)
 class TimeEntryAdmin(admin.ModelAdmin):
-    list_display    = ('id', 'employee_number', 'time_spent', 'invoice_number', 'date')
+    list_display    = ('invoice_number', 'employee_number', 'time_spent', 'notes', 'date', 'id')
     list_filter     = ('date', 'employee_number')
     search_fields   = ('employee_number', 'invoice_number')
